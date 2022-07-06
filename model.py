e18dacfd2933f433c5f42b6a383c97f91d76e9db
Aman <yourid9@gmail.com>
2022-07-06 16:39:05 +0530

    updated

---
+++ model.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.model_selection import train_test_split
+
+import warnings
+warnings.filterwarnings('ignore')
+
+df=pd.read_csv('zomato_data.csv')
+
+#df.drop('Unnamed':0 , axis=1, inplace=True)
+print(df.head())
+x= df.drop('rate', axis=1)
+y =df['rate']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_state=10)
+
+from sklearn.ensemble import ExtraTreesRegressor
+et = ExtraTreesRegressor()
+et.fit(x_train,y_train)
+
+et_pred=et.predict(x_test)
+
+
+import pickle
+pickle.dump(et,open('model.pkl','wb'))
+model = pickle.load(open('model.pkl','rb'))
+print(et_pred)
